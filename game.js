427bb43b07ae399014bba5d5a8b91fa925d8d485
David Hollinger <david.hollinger123@gmail.com>
2022-04-01 18:22:48 -0700

    Edited script to make it more clear

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let gameOverAudio = new Audio("assets/gameover.mp3");
 let clickSound = new Audio("assets/clicksound.mp3");
 clickSound.volume = 0.3;
 gameOverAudio.volume = 0.5;
-var theDojo = [
+var theMap = [
     //  j1 j2 j3 j4 j5 j6 j7 j8 j9 j10
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //i 0
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //i 1
@@ -21,22 +21,22 @@ var dojoDiv = document.querySelector("#the-dojo");
 for (let i = 0; i < 10; i++) {
     let randomNum = Math.floor(Math.random() * 9);
     console.log;
-    theDojo[Math.floor(Math.random() * 9)].fill(1, randomNum, randomNum + 1);
+    theMap[Math.floor(Math.random() * 9)].fill(1, randomNum, randomNum + 1);
 }
 
 // Creates the rows of buttons for this game
-function render(theDojo) {
+function render(theMap) {
     var result = "";
-    for (var i = 0; i < theDojo.length; i++) {
-        for (var j = 0; j < theDojo[i].length; j++) {
+    for (var i = 0; i < theMap.length; i++) {
+        for (var j = 0; j < theMap[i].length; j++) {
             result += `<button class="tatami" onclick="howMany(${i}, ${j}, this)"></button>`;
         }
     }
     return result;
 }
 
-console.log(theDojo);
-// var theDojo = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0], //i 0       og dojo
+console.log(theMap);
+// var theMap = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0], //i 0       og dojo
 //                 [3, 1, 0, 7, 0, 0, 6, 0, 8, 8], //i 1
 //                 [5, 0, 7, 0, 3, 6, 6, 6, 0, 0], //i 2
 //                 [2, 3, 0, 9, 0, 0, 6, 0, 8, 0], //i 3
@@ -51,7 +51,7 @@ console.log(theDojo);
 
 // TODO - Make this function tell us how many ninjas are hiding
 //        under the adjacent (all sides and corners) squares.
-//        Use i and j as the indexes to check theDojo.
+//        Use i and j as the indexes to check theMap.
 function reloadGame() {
     location.reload();
 }
@@ -87,7 +87,7 @@ function howMany(i, j, element) {
       [(i+1,j-1), (i+1, j+1), (i+1, j+1)]
 
   */
-    let clicked = theDojo[i][j];
+    let clicked = theMap[i][j];
     let subArr = [
         { r: i - 1, c: j - 1 },
         { r: i - 1, c: j },
@@ -114,7 +114,7 @@ function howMany(i, j, element) {
             //console.log("invalid -->", subArr[x]);
         } else {
             //console.log("Valid --> ", subArr[x]);
-            newTot += theDojo[subArr[x]["r"]][subArr[x]["c"]];
+            newTot += theMap[subArr[x]["r"]][subArr[x]["c"]];
         }
     }
     element.innerHTML = newTot;
@@ -122,7 +122,7 @@ function howMany(i, j, element) {
 
 // BONUS CHALLENGES
 // 1. draw the number onto the button instead of alerting it
-// 2. at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
+// 2. at the start randomly place 10 ninjas into theMap with at most 1 on each spot
 // 3. if you click on a ninja you must restart the game
 //    dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
 
@@ -132,6 +132,6 @@ var style = "color:cyan;font-size:1.5rem;font-weight:bold;";
 console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style);
 console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style);
 // shows the dojo for debugging purposes
-console.table(theDojo);
+console.table(theMap);
 // adds the rows of buttons into <div id="the-dojo"></div>
-dojoDiv.innerHTML = render(theDojo);
+dojoDiv.innerHTML = render(theMap);
